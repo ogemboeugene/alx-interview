4479d01e54904ac34805f9716cb3fb2db4f547c8
ogemboeugene <brianeugene851@gmail.com>
2024-10-15 15:22:18 -0700

    second

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,7 +1,16 @@
 #!/usr/bin/python3
+'''
+Simple script to calculate the minimum number of operations needed to obtain
+n 'H' characters using the operations Copy All and Paste.
+'''
+
 
 
 def minOperations(n):
+    '''
+    Calculate the minimum number of operations needed to obtain
+    n 'H' characters using the operations Copy All and Paste.
+    '''
     if n <= 1:
         return 0
     
