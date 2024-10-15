3febb891133f840ce8968c8e6edb84b8aedc9093
ogemboeugene <brianeugene851@gmail.com>
2024-10-15 15:18:54 -0700

    second

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -5,20 +5,20 @@ n 'H' characters using the operations Copy All and Paste.
 '''
 
 
+#!/usr/bin/python3
+
+
 def minOperations(n):
-    '''
-    Calculate the minimum number of operations needed to obtain
-    n 'H' characters using the operations Copy All and Paste.
-    '''
-    if n == 1:
-        return 0  # No operations needed for a single 'H'
-
-    dp = [float('inf')] * (n + 1)
-    dp[1] = 0
-
-    for i in range(2, n + 1):
-        for j in range(1, i):
-            if i % j == 0:
-                dp[i] = min(dp[i], dp[j] + i // j)
-
-    return dp[n] if dp[n] != float('inf') else 0
+    if n <= 1:
+        return 0
+    
+    operations = 0
+    factor = 2
+    
+    while n > 1:
+        while n % factor == 0:
+            operations += factor
+            n //= factor
+        factor += 1
+    
+    return operations
