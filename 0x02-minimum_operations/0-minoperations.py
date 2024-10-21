b985e178536d40813b9d811c5ca935c2e836f02f
ogemboeugene <brianeugene851@gmail.com>
2024-10-21 07:21:25 +0000

    interview

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -2,28 +2,22 @@
 """
 Minimum Operations
 """
-
 def minOperations(n):
     """
     Calculates the fewest number of operations needed to result in exactly n H characters in the file.
-
     Parameters:
     n (int): The target number of H characters.
-
     Returns:
     int: The minimum number of operations needed, or 0 if n is impossible to achieve.
     """
     if n <= 1:
         return 0
-
     operations = 0
     divisor = 2
-
     while n > 1:
         while n % divisor == 0:
             operations += divisor
             n //= divisor
         divisor += 1
-
     return operations
 
