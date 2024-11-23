64241181abe586ab739c8e74ad1613b02e3c8d55
ogemboeugene <brianeugene851@gmail.com>
2024-11-23 10:59:31 +0000

    matrix

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,29 +1,23 @@
 #!/usr/bin/python3
-"""2D matrix rotation module.
+"""
+Rotate matrix
 """
 
 
-def rotate_2d_matrix(matrix):
-    """Rotates an m by n 2D matrix in place.
+def rotate_2d_matrix(matrix) -> None:
     """
-    if type(matrix) != list:
-        return
-    if len(matrix) <= 0:
-        return
-    if not all(map(lambda x: type(x) == list, matrix)):
-        return
-    rows = len(matrix)
-    cols = len(matrix[0])
-    if not all(map(lambda x: len(x) == cols, matrix)):
-        return
-    c, r = 0, rows - 1
-    for i in range(cols * rows):
-        if i % rows == 0:
-            matrix.append([])
-        if r == -1:
-            r = rows - 1
-            c += 1
-        matrix[-1].append(matrix[r][c])
-        if c == cols - 1 and r >= -1:
-            matrix.pop(r)
-        r -= 1
+    Rotates a 2D square matrix by 90 degrees clockwise.
+    Args:
+    matrix (list[list]): The input matrix to be rotated.
+    Returns:
+    None; the function modifies the input matrix in-place.
+    """
+    n = len(matrix)
+
+    for i in range(n // 2):
+        for j in range(i, n - i - 1):
+            temp = matrix[i][j]
+            matrix[i][j] = matrix[n - j - 1][i]
+            matrix[n - j - 1][i] = matrix[n - i - 1][n - j - 1]
+            matrix[n - i - 1][n - j - 1] = matrix[j][n - i - 1]
+            matrix[j][n - i - 1] = temp
